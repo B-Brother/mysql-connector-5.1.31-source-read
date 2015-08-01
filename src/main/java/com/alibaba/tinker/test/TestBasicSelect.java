50eef72c95d88342322c26158a3bb4cc12279a4a
B-Brother <464248978@qq.com>
2015-08-01 14:24:21 +0800

    init

---
+++ src/main/java/com/alibaba/tinker/test/TestBasicSelect.java
@@ -0,0 +1,26 @@
+package com.alibaba.tinker.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class TestBasicSelect {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tinker", "root", "beckham");
+		
+		Statement stmt = conn.createStatement();
+	
+		ResultSet rs = stmt.executeQuery("SELECT * FROM REGISTER_INTERFACE");
+		
+		while(rs.next()){
+			String appName = rs.getString("APP_NAME");
+			
+			String timeOut = rs.getString("TIMEOUT");
+			
+			System.out.println("appName:" + appName + ", timeOut=" + timeOut);
+		}
+	}
+}
